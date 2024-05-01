7ecccdbde4e8df79aacc36ce6268d99301930d76
makanta <mktmakantamkt@gmail.com>
2024-05-01 17:37:53 +0100

    adding my project

---
+++ my-project/src/Content.jsx
@@ -0,0 +1,84 @@
+function Content() {
+return (
+  <>
+    <div
+    className=" text-center space-y-6 p-8 font-semiboldbold text-lg  text-slate-300 bg-[url('/src/images/background1.jpg')]">
+        <h3 className="text-2xl ">SO, YOU WANT TO TRAVEL</h3>
+        <h1 className="text-8xl text-white pb-8">SPACE</h1>
+        <p className="pb-16 text-balance text-xl">Lets Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat accusamus ut recusandae dolores placeat labore, molestias molestiae aliquam, error adipisci inventore explicabo optio? Obcaecati doloremque voluptatum debitis, veniam est necessitatibus.</p>
+        <div className="w-40 h-40  bg-white flex justify-center items-center rounded-full mx-auto mb-28 font-semibold text-2xl text-slate-700 ">EPLORE</div>
+    </div>
+    <div>
+        <div>
+          <h3><span>01</span>PICK YOUR DESTINATION</h3>
+          <img src="/src/images/image1.jpg" alt="" />
+        </div>
+        <div>
+          <ul> 
+            <li>MOON</li>
+            <li>MARS</li>
+            <li>EUROPA</li>
+            <li>TITAN</li>
+          </ul>
+          <h1>MARS</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sint labore quidem nihil! Ex odit sed fugit esse excepturi temporibus!</p>
+          <div>
+            <div>
+              <h3>AVG. DISTANCE</h3>
+              <h2>225 MILL.KM</h2>
+            </div>
+            <div>
+              <h3>EST. TRAVEL TIME</h3>
+              <h2>9 MONTHS</h2>
+            </div>
+            
+
+          </div>
+
+        </div>
+    </div>
+    <div>
+        <div>
+          <h3><span>02</span>MEET YOUR CREW</h3>
+          <div>
+            <h3>FLIGHT ENGINEER</h3>
+            <h2>ANOUSHEH ANSARI</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi voluptatum eaque quo animi facilis perferendis molestiae, odio aut iste. Fuga, optio ratione? Veniam, voluptatem quae?</p>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+
+
+    </div>
+    <div>
+      <div>
+        <h3><span>01</span>PICK YOUR DESTINATION</h3>
+        <div>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+          <div>
+            <h5>THE TECHNOLOGY</h5>
+            <h2>LAUNCH VEHICLE</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem accusamus aliquam itaque excepturi quaerat qui expedita nemo, cum ipsam, aut dolore sapiente. Deserunt ea iure vero culpa esse non blanditiis cum! Quae nam perspiciatis doloremque quas.</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <img src="" alt="" />
+      </div>
+    </div>
+  </>
+)
+}
+
+export default Content
---
+++ my-project/src/Header.jsx
@@ -0,0 +1,13 @@
+
+    function Header() {
+return (
+  <>
+    <header className="h-fit w-screen bg-blue-600 text-white flex justify-between px-10 py-6 text-2xl  " >
+      <h1>Move</h1>
+      <div >=</div>
+    </header>
+  </>
+)
+}
+
+export default Header
