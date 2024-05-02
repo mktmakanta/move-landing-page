d0e99db4266c6a1ab7aa8b59f0471bb27dafffaa
makanta <mktmakantamkt@gmail.com>
2024-05-02 21:13:49 +0100

    i have been able to complete the mobile version, moving unto the medium/tablet screen

--- my-project/src/Header.jsx
+++ my-project/src/Header.jsx
@@ -4,8 +4,7 @@ import Navbar from "./Nav.jsx"
 function Header() {
 return (
   <>
-    <header className="h-fit w-screen bg-blue-600 text-white flex justify-between px-10 py-6 text-2xl  " >
-      <h1>Moving</h1>
+    <header >
       <div  >
         <Navbar />
       </div>
--- my-project/src/Nav.jsx
+++ my-project/src/Nav.jsx
@@ -4,19 +4,20 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="">
+    <nav className=" bg-blue-500 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex md:hidden">
+        <div className="flex items-center justify-between h-20">
+          <div className="flex justify-between w-full md:hidden">
+            <h2 className='text-2xl font-bold'>Moving</h2>
             <button 
               onClick={() => setIsOpen(!isOpen)}
-              className="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset focus:ring-white"
+              className="inline-flex items-center justify-center p-2 rounded-md  hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
               <span className="sr-only">Open main menu</span>
               {!isOpen ? (
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white " fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
               ) : (
@@ -27,23 +28,26 @@ const Navbar = () => {
             </button>
           </div>
           <div className="hidden md:block">
+            <h2>Moving</h2>
             <div className="flex space-x-4">
               <a href="#" className="px-3 py-2 text-white">Home</a>
-              <a href="#" className="px-3 py-2 text-white">About</a>
+              <a href="#" className="px-3 py-2 text-white">Destination</a>
+              <a href="#" className="px-3 py-2 text-white">Our crew</a>
               <a href="#" className="px-3 py-2 text-white">Services</a>
-              <a href="#" className="px-3 py-2 text-white">Contact</a>
             </div>
           </div>
         </div>
       </div>
       {isOpen && (
+        <div>
         <div className="md:hidden" id="mobile-menu">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 w-full">
-            <a href="#" className="block px-3 py-2 text-base font-medium text-white hover:text-white hover:bg-gray-700">Home</a>
-            <a href="#" className="block px-3 py-2 text-base font-medium text-white hover:text-white hover:bg-gray-700">About</a>
-            <a href="#" className="block px-3 py-2 text-base font-medium text-white hover:text-white hover:bg-gray-700">Services</a>
-            <a href="#" className="block px-3 py-2 text-base font-medium text-white hover:text-white hover:bg-gray-700">Contact</a>
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3 w-full text-2xl flex flex-col items-center rounded-full">
+            <a href="#" className="block px-24 rounded-full py-2  font-medium text-white hover:text-white hover:bg-blue-400">Home</a>
+            <a href="#" className="block px-24 rounded-full py-2  font-medium text-white hover:text-white hover:bg-blue-400">Destination</a>
+            <a href="#" className="block px-24 rounded-full py-2  font-medium text-white hover:text-white hover:bg-blue-400">Our crew</a>
+            <a href="#" className="block px-24 rounded-full py-2  font-medium text-white hover:text-white hover:bg-blue-400">Services</a>
           </div>
+        </div>
         </div>
       )}
     </nav>
