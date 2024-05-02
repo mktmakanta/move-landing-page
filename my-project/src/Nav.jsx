005fb53ecc988ce1da32a21fbf0c0a43ff5b2232
makanta <mktmakantamkt@gmail.com>
2024-05-02 22:02:10 +0100

    done with the tablet screen version, moving unto the large/desktop screen version

--- my-project/src/Nav.jsx
+++ my-project/src/Nav.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
   return (
     <nav className=" bg-blue-500 text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-20">
+        <div className="flex items-center justify-between  h-20">
           <div className="flex justify-between w-full md:hidden">
-            <h2 className='text-2xl font-bold'>Moving</h2>
+            <h2 className='text-2xl font-bold'>Space</h2>
             <button 
               onClick={() => setIsOpen(!isOpen)}
               className="inline-flex items-center justify-center p-2 rounded-md  hover:text-white hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset focus:ring-white"
@@ -17,20 +17,20 @@ const Navbar = () => {
             >
               <span className="sr-only">Open main menu</span>
               {!isOpen ? (
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white " fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 text-white " fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
               ) : (
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={3} d="M6 18L18 6M6 6l12 12" />
                 </svg>
               )}
             </button>
           </div>
-          <div className="hidden md:block">
-            <h2>Moving</h2>
-            <div className="flex space-x-4">
-              <a href="#" className="px-3 py-2 text-white">Home</a>
+          <div className="hidden md:flex justify-between md:w-full md:text-2xl">
+            <h2 className='text-4xl'>Space</h2>
+            <div className="flex space-x-4 ">
+              <a href="#" className="px-3 py-2 text-white  ">Home</a>
               <a href="#" className="px-3 py-2 text-white">Destination</a>
               <a href="#" className="px-3 py-2 text-white">Our crew</a>
               <a href="#" className="px-3 py-2 text-white">Services</a>
